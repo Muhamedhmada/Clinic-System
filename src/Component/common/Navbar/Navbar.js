b00245714ca1c3f51574256bb02b8a94cb3f8b54
Muhamedhmada <mohamed_31139780@ics.tanta.edu.eg>
2025-11-10 19:58:05 +0200

    notification menu style

--- src/Component/common/Navbar/Navbar.js
+++ src/Component/common/Navbar/Navbar.js
@@ -5,22 +5,26 @@ function Navbar() {
   const navBarRef = useRef();
   const [isFixed, setIsFixed] = useState(false);
 
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        setIsFixed(!entry.isIntersecting);
-      },
-      {threshold: 0.1}
-    );
-    if (navBarRef.current) {
-      observer.observe(navBarRef.current);
-    }
-  });
+useEffect(() => {
+  const observer = new IntersectionObserver(
+    ([entry]) => {
+      const newState = !entry.isIntersecting;
+      if (navBarRef.current !== newState) {
+        setIsFixed(newState);
+        navBarRef.current = newState;
+      }
+    },
+    { threshold: 0.1 }
+  );
+
+  if (navBarRef.current) observer.observe(navBarRef.current);
+  return () => observer.disconnect();
+}, []);
 
   return (
     <>
       {isFixed && (
-        <div className='navbar-container container animated'>
+        <div className={isFixed?"navbar-container container animated":"navbar-container container"} ref={navBarRef}>
           <NavComp/>
         </div>
       )}
--- src/Component/common/Notification/Notification.js
+++ src/Component/common/Notification/Notification.js
@@ -3,20 +3,21 @@ import {Notification as NotIcaon} from "../../../Assets/SVGS";
 import Icon from '../Icon/Icon';
 import { useEffect, useState } from 'react';
 import getData from '../../../utils/getData';
-import {motion } from 'framer-motion'
+import {motion , AnimatePresence } from 'framer-motion'
 import logo from '../../../Assets/Images/clinicLogo.png'
+import { notification } from 'antd';
 function Notification(){
   const [myNotification , setMyNotification] = useState([])
-  const [isOpen , setIsOpen] = useState(true)
+  const [isOpen , setIsOpen] = useState(false)
 
   const handleNotificationIcon = ()=>{
-    setIsOpen(true)
+    setIsOpen((prev)=>!prev)
   }
   const getNotification = async()=>{
     const res = await getData('notifications/my-notifications' )
-    console.log(res.data.data[0].message)
-    console.log(res.data.data[0].type)
-    setMyNotification(res.data.data)
+    console.log(res?.data?.data[0]?.message)
+    console.log(res?.data?.data[0]?.type)
+    setMyNotification(res?.data?.data)
   }
   useEffect(()=>{
     getNotification()
@@ -24,32 +25,37 @@ function Notification(){
   return(
     <div className="notification-container">
       <Icon icon={<NotIcaon width="20px"/>} func={handleNotificationIcon}/>
+      <AnimatePresence>
       {
         isOpen && 
-        <motion.div
-          key={{isOpen}}
-          initial={{translateX:100}}
-          animate={{translateX:0}}
-          transition={{duration: 0.2}}
-          className="notifications">
-            <h3>notifications</h3>
-          {
-            myNotification.map((item)=>(
-              <div className='notification'>
-                <div>
-                  <h5>
-                    {item.type}
-                  </h5>
-                  <p>{item.message.slice(0,35)}</p>
+          <motion.div
+            key={{isOpen}}
+            initial={{translateX:100}}
+            animate={{translateX:0}}
+            transition={{duration: 0.2}}
+            className="notifications">
+              <h3>notifications</h3>
+            {
+              notification.length !== 0?(
+
+                myNotification?.map((item)=>(
+                  <div className='notification'>
+                    <div>
+                      <h5>
+                        {item?.type}
+                      </h5>
+                      <p>{item?.message.slice(0,35)}</p>
+                    </div>
+                    <div className="image">
+                      <img src={logo} alt="" />
+                    </div>
                 </div>
-                <div className="image">
-                  <img src={logo} alt="" />
-                </div>
-            </div>
-            ))
-          }
-        </motion.div>
+                ))
+              ):"no data to preview"
+            }
+          </motion.div>
       }
+      </AnimatePresence>
     </div>
   )
 }
